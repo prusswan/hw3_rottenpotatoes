3b7b1599aa31e1b675d9dad4ac561cc072a7ac72
prusswan <prusswan@gmail.com>
2012-03-16 03:14:24 +0800

    completed hw3 Part 1: Create a declarative scenario step for adding movies

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,13 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  assert false, "Unimplmemented"
+  #assert false, "Unimplmemented"
+end
+
+Given /^I am on the RottenPotatoes home page$/ do
+  visit('/movies')
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -26,3 +31,4 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
+
